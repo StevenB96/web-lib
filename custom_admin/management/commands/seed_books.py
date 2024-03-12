ac69251dca47f66549c1b0169d7b3d97918f3fae
StevenB96 <62147124+StevenB96@users.noreply.github.com>
2024-03-12 01:04:03 +0000

    Improved book data import

--- custom_admin/management/commands/seed_books.py
+++ custom_admin/management/commands/seed_books.py
@@ -30,9 +30,22 @@ class Command(BaseCommand):
         ]
         book_df_B = pd.read_csv(book_data_B_path, usecols=columns_to_read_B)
 
+        # Remove duplicates from both dataframes
+        book_df_A.drop_duplicates(subset=['original_title'], inplace=True)
+        book_df_B.drop_duplicates(subset=['title'], inplace=True)
+
+        # Convert columns to lowercase for case-insensitive matching
+        book_df_A['original_title_lc'] = book_df_A['original_title'].str.lower()
+        book_df_B['title_lc'] = book_df_B['title'].str.lower()
+
         # Merge the DataFrames on the specified columns
         merged_df = pd.merge(
-            book_df_A, book_df_B, left_on='original_title', right_on='title', how='inner')
+            book_df_A, 
+            book_df_B, 
+            left_on='original_title_lc', 
+            right_on='title_lc', 
+            how='inner'
+        )
 
         # Remove rows with any empty values in any column
         book_df_A.dropna(inplace=True)
