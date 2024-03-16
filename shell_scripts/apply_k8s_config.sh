b4f0917f15c5292c37d5fd4adbc8ee1570a1572b
StevenB96 <stevenberrisford@gmail.com>
2024-03-16 00:47:59 +0000

    k8s improvements

--- shell_scripts/apply_k8s_config.sh
+++ shell_scripts/apply_k8s_config.sh
@@ -4,9 +4,9 @@ kubectl apply -f ./k8s/configmap/web-lib-configmap.yaml
 # Secret
 kubectl apply -f ./k8s/secret/mysql-db-secret.yaml
 kubectl apply -f ./k8s/secret/web-lib-secret.yaml
-# Deployment
-kubectl apply -f ./k8s/deployment/mysql-db-deployment.yaml
-kubectl apply -f ./k8s/deployment/web-lib-deployment.yaml
 # Service
 kubectl apply -f ./k8s/service/mysql-db-service.yaml
 kubectl apply -f ./k8s/service/web-lib-service.yaml
+# Deployment
+kubectl apply -f ./k8s/deployment/mysql-db-deployment.yaml
+kubectl apply -f ./k8s/deployment/web-lib-deployment.yaml
--- shell_scripts/delete_k8s_config.sh
+++ shell_scripts/delete_k8s_config.sh
@@ -4,9 +4,9 @@ kubectl delete -f ./k8s/configmap/web-lib-configmap.yaml
 # Secret
 kubectl delete -f ./k8s/secret/mysql-db-secret.yaml
 kubectl delete -f ./k8s/secret/web-lib-secret.yaml
-# Deployment
-kubectl delete -f ./k8s/deployment/mysql-db-deployment.yaml
-kubectl delete -f ./k8s/deployment/web-lib-deployment.yaml
 # Service
 kubectl delete -f ./k8s/service/mysql-db-service.yaml
 kubectl delete -f ./k8s/service/web-lib-service.yaml
+# Deployment
+kubectl delete -f ./k8s/deployment/mysql-db-deployment.yaml
+kubectl delete -f ./k8s/deployment/web-lib-deployment.yaml
